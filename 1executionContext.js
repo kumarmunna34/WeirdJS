6511c3cbf82b4b0ab4d813c28b3e6f1363b050a9
kumarmunna34 <47803726+kumarmunna34@users.noreply.github.com>
2025-01-01 02:02:30 +0530

    scope chain and block scope

--- 1executionContext.js
+++ 1executionContext.js
@@ -1,12 +1,34 @@
-function B() {
-  var myVar;
-  console.log(myVar);
-}
-function A() {
+// function B() {
+//   var myVar;
+//   console.log(myVar);
+// }
+// function A() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   B();
+// }
+// var myVar = 1;
+// console.log(myVar);
+// A();
+
+//Scope Chain
+
+function a() {
+  function b() {
+    console.log(myVar);
+  }
   var myVar = 2;
-  console.log(myVar);
-  B();
+  b();
 }
 var myVar = 1;
-console.log(myVar);
-A();
+a();
+
+//ES6 Block scoping
+// Q. what is the output?
+
+if (6 > 5) {
+  console.log(a);
+  console.log(b);
+  var a = true;
+  let b = true; // block scope applies to let not var
+}
